6bf4a667cbe0716bfac5810a682ae736a8f59f7a
jacopo713 <gymlionshop@gmail.com>
2025-01-09 08:30:48 +0100

    Added authentication system

--- app/lib/firebaseAdmin.ts
+++ app/lib/firebaseAdmin.ts
@@ -5,12 +5,12 @@ import admin from 'firebase-admin';
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      projectId: process.env.FIREBASE_ADMIN_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_ADMIN_CLIENT_EMAIL,
       // Sostituisce le sequenze di escape delle nuove linee con vere nuove linee
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+      privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    databaseURL: process.env.FIREBASE_ADMIN_DATABASE_URL,
   });
 }
 
