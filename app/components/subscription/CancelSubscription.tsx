90e38f360613b929eb66d969af7da93654c2b7c0
jacopo713 <gymlionshop@gmail.com>
2025-01-10 15:12:18 +0100

    vercel

--- app/components/subscription/CancelSubscription.tsx
+++ app/components/subscription/CancelSubscription.tsx
@@ -1,7 +1,7 @@
 // app/components/subscription/CancelSubscription.tsx
 'use client';
 
-import React, { useState, Fragment } from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { useAuth } from '@/app/contexts/AuthContext';
 
