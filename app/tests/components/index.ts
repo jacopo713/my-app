2dad96fc792ce4798cb18afa5800c4bab8d6a72d
jacopo713 <gymlionshop@gmail.com>
2025-01-11 02:02:02 +0100

    vercel

--- app/tests/components/index.ts
+++ app/tests/components/index.ts
@@ -3,13 +3,12 @@ import EyeHandTest from './EyeHand';
 import StroopTest from './Stroop';
 import SpeedReadingTrainer from './SpeedReading';
 import ShortTermMemoryTest from './ShortTermMemoryTest';
-import SchulteTable, { SchulteTableProps } from './SchulteTable'; // Importa il nuovo componente
+import SchulteTable from './SchulteTable'; // Importa il nuovo componente
 
 // Export interfaces
 export type { RavenTestProps } from './Raven';
 export type { EyeHandTestProps } from './EyeHand';
 export type { SpeedReadingTrainerProps } from './SpeedReading';
-export type { SchulteTableProps }; // Esporta l'interfaccia del nuovo componente
 
 // Export components
 export {
