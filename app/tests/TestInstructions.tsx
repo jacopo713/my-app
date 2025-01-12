8a1e8b9504c5e10b6a4c9406f6c4afe05871dff4
jacopo713 <gymlionshop@gmail.com>
2025-01-12 08:35:21 +0100

    vercel

--- app/tests/TestInstructions.tsx
+++ app/tests/TestInstructions.tsx
@@ -15,7 +15,7 @@ export interface TestInstructions {
   iconColor?: string; // Colore dell'icona
 }
 
-export type TestPhase = "intro" | "ravenTest" | "eyehandTest" | "stroopTest" | "speedreadingTest" | "memoryTest" | "schulteTest" | "rhythmTest" | "resultsTest";
+export type TestPhase = "intro" | "raven" | "eyehand" | "stroop" | "speedreading" | "memory" | "schulte" | "rhythm" | "results";
 
 export const testInstructions: Record<TestPhase, TestInstructions> = {
   intro: {
@@ -279,4 +279,5 @@ export const TestInstructionsComponent: React.FC<TestInstructionsProps> = ({ pha
     </div>
   );
 };
+
 
