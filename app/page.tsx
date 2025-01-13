65f0640ad313f8e4845583032caade36975dec42
jacopo713 <gymlionshop@gmail.com>
2025-01-13 23:54:56 +0100

    vercel

--- app/page.tsx
+++ app/page.tsx
@@ -14,20 +14,13 @@ import {
   BarChart,
 } from 'lucide-react';
 import GlobalRanking from '@/app/components/GlobalRanking'; // Importa il componente GlobalRanking
-import { useAuth } from '@/app/contexts/AuthContext'; // Importa il contesto di autenticazione
 
 const HomePage: React.FC = () => {
   const router = useRouter();
-  const { user } = useAuth(); // Ottieni lo stato dell'utente
 
   const handleStartTest = () => {
-    if (!user) {
-      // Se l'utente non è loggato, reindirizza alla pagina di registrazione
-      router.push('/register');
-    } else {
-      // Se l'utente è loggato, reindirizza alla pagina dei test
-      router.push('/tests');
-    }
+    // Reindirizza direttamente alla pagina dei test
+    router.push('/tests');
   };
 
   return (
